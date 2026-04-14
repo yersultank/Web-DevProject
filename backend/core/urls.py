e675c5809a3074b1636af3a05c286f6aa9653ecf
Yersultan <ersultank1010@gmail.com>
2026-04-14 22:56:29 +0500

    fix: backend config for JWT and CORS connectivity

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('assets.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
 ]
