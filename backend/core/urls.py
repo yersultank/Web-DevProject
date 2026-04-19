9c7cabe7cc6f1c23bf800914747bbe4c6577f7b9
Yersultan <ersultank1010@gmail.com>
2026-04-19 15:59:47 +0500

    fix: add JWT auth endpoints to urls.py

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/', include('assets.urls')),
 ]
