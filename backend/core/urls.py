63e01e93b2e64c0c08715ade5242fb198b20723d
Yersultan <ersultank1010@gmail.com>
2026-04-21 19:25:45 +0500

    backend changes for last models

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -22,7 +22,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/', include('assets.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
