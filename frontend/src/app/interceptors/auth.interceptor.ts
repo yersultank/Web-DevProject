faa3cec802c8eb8c95ad2831581d57381a5fc99a
Yersultan <ersultank1010@gmail.com>
2026-04-21 19:21:51 +0500

    fixed auth

--- frontend/src/app/interceptors/auth.interceptor.ts
+++ frontend/src/app/interceptors/auth.interceptor.ts
@@ -4,12 +4,9 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const token = localStorage.getItem('access_token');
 
   if (token) {
-    const cloned = req.clone({
-      setHeaders: {
-        Authorization: `Bearer ${token}`
-      }
+    req = req.clone({
+      setHeaders: { Authorization: `Bearer ${token}` }
     });
-    return next(cloned);
   }
 
   return next(req);
