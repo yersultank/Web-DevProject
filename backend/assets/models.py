5ddac525607e88de9d6eb3e4a145da0b352319a8
Yersultan <ersultank1010@gmail.com>
2026-04-25 17:31:48 +0500

    add UserProfile model

--- backend/assets/models.py
+++ backend/assets/models.py
@@ -1,1 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    full_name = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return f'Profile of {self.user.username}'
