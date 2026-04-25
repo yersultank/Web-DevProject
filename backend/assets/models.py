18a5ee0130f65c13107968b8f3a98ccc9182cb80
Yersultan <ersultank1010@gmail.com>
2026-04-25 19:20:19 +0500

    add course structure models

--- backend/assets/models.py
+++ backend/assets/models.py
@@ -7,3 +7,34 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return f'Profile of {self.user.username}'
+    
+class Course(models.Model):
+    name = models.CharField(max_length=255, default='JavaScript Course')
+
+    def __str__(self):
+        return self.name
+
+
+class Topic(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='topics')
+    title = models.CharField(max_length=255)
+    order = models.IntegerField(default=0)
+
+    class Meta:
+        ordering = ('order',)
+
+    def __str__(self):
+        return self.title
+
+
+class Lesson(models.Model):
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE, related_name='lessons')
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    order = models.IntegerField(default=0)
+
+    class Meta:
+        ordering = ('order',)
+
+    def __str__(self):
+        return self.title
